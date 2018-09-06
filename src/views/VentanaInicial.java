248761ef979a0589442cca94f5fde0407eb5e259
Edgar <Edgar@DESKTOP-6LL1DJT>
2018-09-06 12:31:20 -0300

    listado productos

--- src/views/VentanaInicial.java
+++ src/views/VentanaInicial.java
@@ -44,9 +44,10 @@ public class VentanaInicial extends JFrame {
 	
 	public VentanaInicial()
 	{
+		//iniciarVentanaInicial();
+		//sisCin = SistemaCine.getInstance();
 		if(getStub())
 		{
-		//sisCin = SistemaCine.getInstance();
 		iniciarVentanaInicial();
 		}
 	}
@@ -54,7 +55,7 @@ public class VentanaInicial extends JFrame {
     public boolean getStub() {
     	
     	try {
-			sisCin = (TDASistemaCine)Naming.lookup ("//localhost/SistemaCine");
+			sisCin = (TDASistemaCine)Naming.lookup ("//192.168.157.101/SistemaCine");
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -99,16 +100,40 @@ public class VentanaInicial extends JFrame {
 		{
 			public void actionPerformed(ActionEvent arg0) 
 			{
-				//VentasAnteriores va = new VentasAnteriores(sisCin);
-				//va.setVisible(true);
+				VentasAnteriores va;
+				try {
+					va = new VentasAnteriores(sisCin);
+					va.setVisible(true);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		JButton btnProductos = new JButton("Productos");
+		btnProductos.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				ListadoProductos lp;
+				try {
+					lp = new ListadoProductos(sisCin);
+					lp.setVisible(true);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnProductos, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 333, Short.MAX_VALUE)
 						.addComponent(btnVentasAnteriores, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 333, Short.MAX_VALUE)
 						.addComponent(btnRegistrarVenta, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 333, Short.MAX_VALUE))
 					.addContainerGap())
@@ -119,8 +144,10 @@ public class VentanaInicial extends JFrame {
 					.addGap(27)
 					.addComponent(btnRegistrarVenta, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
-					.addComponent(btnVentasAnteriores, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(105, Short.MAX_VALUE))
+					.addComponent(btnVentasAnteriores, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnProductos, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(42, Short.MAX_VALUE))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
